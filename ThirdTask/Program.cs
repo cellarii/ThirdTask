6e52b1520b9c52272ffb4615baa71b1b10608fcd
Daria <zerlovad@gmail.com>
2026-02-25 12:49:49 +0800

    result for verification

--- ThirdTask/Program.cs
+++ ThirdTask/Program.cs
@@ -1,10 +1,30 @@
 ﻿namespace ThirdTask
 {
+    public class Logic
+    {
+        public static double Calculation(string str)
+        {
+            var count = 0;
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (Char.IsLetter(str[i]))
+                {
+                    count++;
+                }
+            }
+            return Math.Round((double)count / str.Length * 100, 2);
+        }
+    }
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //Дано предложение. Определить долю (в %) букв в нем.
+            Console.Write("Введите предложение ... ");
+            string str = Console.ReadLine();
+            
+            var percent = Logic.Calculation(str);
+            Console.WriteLine("Доля букв в ашем предложении: "+percent);
         }
     }
 }
---
+++ ThirdTaskTests/LogicTests.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ThirdTask;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThirdTask.Tests
+{
+    [TestClass()]
+    public class LogicTests
+    {
+        [TestMethod()]
+        public void OnlyLetters()
+        {
+            var str = "Тест";
+            
+            var result=Logic.Calculation(str);
+
+            Assert.AreEqual(100, result);
+        }
+
+        [TestMethod()]
+        public void SpaceSeparator()
+        {
+            var str = "Тест тест";
+
+            var result = Logic.Calculation(str);
+
+            Assert.AreEqual(88.89, result);
+        }
+
+        [TestMethod()]
+        public void MultipleSpaces()
+        {
+            var str = "Тест   тест";
+
+            var result = Logic.Calculation(str);
+
+            Assert.AreEqual(72.73, result);
+        }
+
+        [TestMethod()]
+        public void SeparatorPunctuationMark()
+        {
+            var str = "Тест,тест";
+
+            var result = Logic.Calculation(str);
+
+            Assert.AreEqual(88.89, result);
+        }
+
+        [TestMethod()]
+        public void MultiplePunctuationMark()
+        {
+            var str = "Тест,!^тест";
+
+            var result = Logic.Calculation(str);
+
+            Assert.AreEqual(72.73, result);
+        }
+
+        [TestMethod()]
+        public void BasicCase()
+        {
+            var str = "Тест, который выполнится!";
+
+            var result = Logic.Calculation(str);
+
+            Assert.AreEqual(84, result);
+        }
+
+    }
+}
